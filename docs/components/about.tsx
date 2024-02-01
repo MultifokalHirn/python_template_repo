20e637e118bbbdc500f189f66090f519468ca798
Lennard Wolf <7870758+MultifokalHirn@users.noreply.github.com>
2024-02-01 22:03:29 +0100

    feat: adds vocs documentation

---
+++ docs/components/about.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export default function About() {
+  return (
+    <div>
+      <img src="/apple-touch-icon.png" />; Lorem Ipsum
+      {/* <img src="/logo.svg" /> */}
+    </div>
+  );
+}
---
+++ vocs.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  description: "Lorem ipsum.",
+  title: "python_template_repo",
+});
