f7cb060d21fc63e5556c680581c4070bb1db711e
Lennard Wolf <7870758+MultifokalHirn@users.noreply.github.com>
2024-02-01 23:44:05 +0100

    docs: figuring more out about vocs use

--- docs/components/footer.tsx
+++ docs/components/footer.tsx
@@ -3,8 +3,8 @@ import * as React from "react";
 export function Footer() {
   return (
     <div className="footer">
-      <div>Released under the MIT License.</div>
-      <div>Copyright © 2024-present Lennard Wolf.</div>
+      {/* <div>Released under the MIT License.</div> */}
+      <div>Copyright © 2024 Lennard Wolf.</div>
     </div>
   );
 }
--- vocs.config.ts
+++ vocs.config.ts
@@ -1,6 +1,33 @@
+import * as React from "react";
+
 import { defineConfig } from "vocs";
 
 export default defineConfig({
+  iconUrl: {
+    light: "/safari-pinned-tab.svg",
+    dark: "/safari-pinned-tab.svg",
+  },
+  logoUrl: {
+    light: "/apple-touch-icon.png",
+    dark: "/apple-touch-icon.png",
+  },
   description: "Lorem ipsum.",
   title: "python_template_repo",
+  blogDir: "./pages/blog",
+  socials: [
+    {
+      icon: "github",
+      link: "https://github.com/MultifokalHirn/python_template_repo",
+    },
+  ],
+  sidebar: [
+    {
+      text: "Documentation",
+      link: "/docs",
+    },
+    {
+      text: "Project Structure",
+      link: "/docs/structure",
+    },
+  ],
 });
